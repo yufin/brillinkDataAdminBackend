4a70a9bf2a76ff41a34c16c9f6c0d6deb53a6daa
zhangwenjian <zwj777@live.com>
2022-09-02 21:56:24 +0800

    feat✨:  删除暂时不用的参数

--- cmd/install/server.go
+++ cmd/install/server.go
@@ -9,14 +9,8 @@ import (
 )
 
 var (
-	configYml  string
-	generate   bool
-	goAdmin    bool
-	host       string
-	SystemName string
-	Username   string
-	Password   string
-	StartCmd   = &cobra.Command{
+	//configYml  string
+	StartCmd = &cobra.Command{
 		Use:     "install",
 		Short:   "项目初始化安装；针对配置文件和数据结构初始化；",
 		Example: "go run main.go install",
@@ -26,12 +20,12 @@ var (
 	}
 )
 
-// fixme 在您看不见代码的时候运行迁移，我觉得是不安全的，所以编译后最好不要去执行迁移
 func init() {
-	StartCmd.PersistentFlags().StringVarP(&configYml, "config", "c", "config/settings.yml", "Start server with provided configuration file")
+	// TODO: 是否需要待定...
+	//StartCmd.PersistentFlags().StringVarP(&configYml, "config", "c", "config/settings.yml", "Start server with provided configuration file")
 }
 
 func setup() {
 	// 检查是否是首次登陆使用
-	startup.Startup(configYml)
+	startup.Startup()
 }
