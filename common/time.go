ab680027fe8c3c9729c156bb670097b22216e1b9
wenjianzhang <zwj777@live.com>
2023-06-12 09:07:44 +0800

    refactor🎨: 添加time格式化函数

--- common/time.go
+++ common/time.go
@@ -8,3 +8,27 @@ func GetDiffDays(t1, t2 time.Time) int {
 
 	return int(t1.Sub(t2).Hours() / 24)
 }
+
+type Time time.Time
+
+const (
+	timeFormat = "2006-01-02 15:04:05"
+)
+
+func (t *Time) UnmarshalJSON(data []byte) (err error) {
+	now, err := time.ParseInLocation(`"`+timeFormat+`"`, string(data), time.Local)
+	*t = Time(now)
+	return
+}
+
+func (t Time) MarshalJSON() ([]byte, error) {
+	b := make([]byte, 0, len(timeFormat)+2)
+	b = append(b, '"')
+	b = time.Time(t).AppendFormat(b, timeFormat)
+	b = append(b, '"')
+	return b, nil
+}
+
+func (t Time) String() string {
+	return time.Time(t).Format(timeFormat)
+}
